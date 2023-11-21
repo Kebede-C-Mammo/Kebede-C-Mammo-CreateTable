105728eb458a4b628878408d591bdf4b4602edf5
Kebede Mammo <mkebede2020@gmail.com>
2023-11-21 01:08:22 +0000

    Complete createTable coding lab.

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-
+CREATE TABLE song (title VARCHAR(100) NOT NULL,artist VARCHAR(100) NOT NULL); INSERT INTO song (title, artist) VALUES ('Let it be', 'Beatles'); INSERT INTO song (title, artist) VALUES ('Hotel California', 'Eagles'); INSERT INTO song (title, artist) VALUES ('Kashmir', 'Led Zeppelin');
